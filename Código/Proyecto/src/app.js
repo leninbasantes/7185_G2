aeea70f402690d49c1aaf1583eeeaaae15d19589
Lenin <leninbasantes@hotmail.com>
2022-01-11 23:31:14 -0500

    Create Login

--- Código/Proyecto/src/app.js
+++ Código/Proyecto/src/app.js
@@ -13,7 +13,7 @@ mongoose.connect('mongodb+srv://web:12345@cluster0.u9amb.mongodb.net/multifiltro
 const indexRoutes = require('./routes');
 
 //settings of server
-app.set('port', process.env.PORT || 3001);
+app.set('port', 3001);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
